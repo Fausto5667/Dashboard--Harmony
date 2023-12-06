aa483c9cd786e040c0d096afcea7948cf9ff8bbf
Matheuuuuuuuus <126706433+Matheuuuuuuuus@users.noreply.github.com>
2023-12-06 15:30:43 -0300

    Add files via upload

---
+++ cad-listar.php
@@ -0,0 +1,8 @@
+<?php
+$titulo_pagina = "Cadastro usuario";
+require_once "./template/header.php";
+require_once "./template/navbar.php";
+require_once "./template/sidebar.php";
+require_once "./template/main-cad-listar.php";
+require_once "./template/footer.php";
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$titulo_pagina = "Painel de administração";
+require_once "./template/header.php";
+require_once "./template/navbar.php";
+require_once "./template/sidebar.php";
+require_once "./template/footer.php";
+?>
